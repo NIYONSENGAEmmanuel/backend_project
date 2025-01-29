a659c56524ba70153c90a4cb33d339ffe629bf39
John Niyontwali <nijohn0006@gmail.com>
2025-01-30 01:37:08 +0200

    refined the codebase

--- src/controllers/notes/getNotes.js
+++ src/controllers/notes/getNotes.js
@@ -3,7 +3,7 @@ const Note = require('../../database/models/note');
 const getNotes = async (req, res) => {
   try {
     // Add user filter to only get notes for the logged-in user
-    const notes = await Note.find({ _id: req.user.id }).sort({ createdAt: -1 });
+    const notes = await Note.find({ userId: req.user.id }).sort({ createdAt: -1 });
 
     return res.status(200).json({
       ok: true,
